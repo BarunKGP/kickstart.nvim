7bce4658edbb771419518ea228c3cb019d4c75e5
Barun Das <barun.das@autodesk.com>
2024-08-29 15:15:47 -0400

    fix themes, remove neo-tree

--- init.lua
+++ init.lua
@@ -176,7 +176,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'everforest',
         component_separators = '|',
         section_separators = '',
       },
